8003f0a518d0bd6d0eeee074b8c15175a273853b
Julieta Pailler <jepailler@gmail.com>
2022-07-13 18:35:55 -0300

    tabla novedades - listar

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../../models/novedadesModel');
 
-router.get('/', function(req,res,next){
+router.get('/', async function(req,res,next){
+    var novedades = await novedadesModel.getNovedades();
     res.render('admin/novedades',{
         layout:'admin/layout',//admin/layout.hbs
-        persona: req.session.nombre //le paso una propiedad q en este caso esta almacenando el nombre (Flavia)
+        persona: req.session.nombre,
+        novedades //le paso una propiedad q en este caso esta almacenando el nombre (Flavia)
     });//view/admin/novedades.hbs
 }); //cierro el GET
 
