19b610b1537a4c08521236a1c8b692333bbf92f4
Julieta Pailler <jepailler@gmail.com>
2022-07-13 21:49:53 -0300

    diseño y query agregar novedad

--- models/novedadesModel.js
+++ models/novedadesModel.js
@@ -7,4 +7,17 @@ async function getNovedades(){
 
 }
 
-module.exports = { getNovedades }
+async function insertNovedades(obj){
+    try{
+        var query ='insert into novedades set ?';
+        var rows = await pool.query(query,[obj]);
+        return rows;
+
+    }catch(error){
+        comsole.log(error);
+        //throw error;//definir el error que va a ir poc dos caminos. Tienen qu estar todos los campos completos
+    }
+
+}
+
+module.exports = { getNovedades, insertNovedades }
--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var novedadesModel = require('../../models/novedadesModel');
 
+/*diseño y listado de novedades*/
 router.get('/', async function(req,res,next){
     var novedades = await novedadesModel.getNovedades();
     res.render('admin/novedades',{
@@ -10,5 +11,17 @@ router.get('/', async function(req,res,next){
         novedades //le paso una propiedad q en este caso esta almacenando el nombre (Flavia)
     });//view/admin/novedades.hbs
 }); //cierro el GET
+
+// funciones de flecha => o poner function, es lo mismo escribir la palabra function antes de los parámetros o poner la flechita después de los parámetros
+//esto sirve para dar de alta las novedades desde la página
+router.get('/agregar',(req,res,next) => {
+    res.render('admin/agregar',{ //agregar.hbs
+        layout: 'admin/layout'
+
+    })
+
+})
+
+
 
 module.exports = router;
