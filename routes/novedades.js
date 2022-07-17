3fece3ddd4ea82494693ce0212a462db82b32d61
Julieta Pailler <jepailler@gmail.com>
2022-07-17 13:30:04 -0300

    novedades desde el front

--- routes/novedades.js
+++ routes/novedades.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../models/novedadesModel'); //para llegar a la query que necesitamos para que novedades de la web interactue con la base de datos
 
-router.get('/', function(req,res,next){
+router.get('/', async function(req,res,next){
+
+    var novedades =await novedadesModel.getNovedades();//genere una variable donde tengo la query
+
     res.render('novedades',{
-        isNovedades:true
+        isNovedades:true,
+        novedades //tengo los registros para poder imprimirlos en el hbs
     });//view/novedades.hbs
 })
 
