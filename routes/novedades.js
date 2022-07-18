151d826ce56aec5a9ffaba993574bfba874c5840
Julieta Pailler <jepailler@gmail.com>
2022-07-18 20:12:46 -0300

    buscador en front

--- routes/novedades.js
+++ routes/novedades.js
@@ -4,12 +4,24 @@ var novedadesModel = require('../models/novedadesModel'); //para llegar a la que
 
 router.get('/', async function(req,res,next){
 
-    var novedades =await novedadesModel.getNovedades();//genere una variable donde tengo la query
+    // var novedades =await novedadesModel.getNovedades();//genere una variable donde tengo la query. Comentamos esta variable porque agregamos el buscador
 
+    var novedades;
+    if(req.query.q === undefined){
+        novedades = await novedadesModel.getNovedades();
+    }else{
+        novedades = await novedadesModel.buscarNovedades(req.query.q);
+    }
+    
     res.render('novedades',{
         isNovedades:true,
-        novedades //tengo los registros para poder imprimirlos en el hbs
+        novedades, //tengo los registros para poder imprimirlos en el hbs
+        is_search: req.query.q !== undefined,
+        q:req.query.q
     });//view/novedades.hbs
 })
+
+
+
 
 module.exports = router;
